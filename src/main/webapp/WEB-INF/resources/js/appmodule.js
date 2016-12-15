af55846ff5510481a5b9927c4763491f09535bcd
Binayak <Binayak@v-lappy-win8>
2016-12-15 00:57:59 -0500

    added some directives, and first draft template for songsInfo

--- src/main/webapp/WEB-INF/resources/js/appmodule.js
+++ src/main/webapp/WEB-INF/resources/js/appmodule.js
@@ -1,5 +1,43 @@
 var myApp = angular.module("myApp", []);
-myApp.controller ("MyController", function ($scope) {
+myApp.factory ('urlService', ['$http', function($http) {
+	var retObject = {};
+	
+	retObject.getData = function() {
+		return $http.get(getContextUrl() + "/rest/message");
+	};
+	
+	return retObject;
+}]);
+
+myApp.controller ("MyController", ['$scope', 'urlService', function ($scope, urlService) {
 	$scope.hello = "Hello";
 	$scope.world = "World!";
+	
+	var ourDataHandler = function (httpResponse) {
+		$scope.songsData = httpResponse.data;
+		console.log($scope.songsData);
+	};
+	urlService.getData().then(ourDataHandler);
+}]);
+
+myApp.directive('songInfo', function() {
+	var returnObject = {};
+	
+	returnObject.restrict = 'AE';
+	returnObject.template = '<button>{{song.songName}}</button>';
+	returnObject.scope = {song: "="};
+	
+	return returnObject;
+});
+
+myApp.directive('datePicker', function() {
+	var returnObject = {};
+	
+	returnObject.restrict = 'AE';
+	returnObject.template = '<input type="text"></input>';
+	returnObject.link = function (scope, elem, attrs) {
+		jQuery(elem.find('input')).datepicker();
+	};
+	
+	return returnObject;
 });
